68060d7baf00bae52531ee635877bf12a8df6283
okosival <okosival@gmail.com>
2021-06-04 21:41:36 +0100

    created a data base for dsiplaying projects done

---
+++ data.js
@@ -0,0 +1,31 @@
+const data = [
+	{
+		Name: "Onstore",
+		description:
+			"An ecommerce web app where clothing accessories like Suit Trousers, Joggers Pants, shirts, shoes and watches can be purchased. ",
+		tools: "React commerce.js Tailwindcss",
+		github: "https://github.com/Valentine-chinedu/onstore_project/tree/master",
+		url: "https://onstore-project.firebaseapp.com/",
+		image: "/onstore screenshot.png",
+	},
+
+	{
+		Name: "SocialHangout",
+		description: "A social media web app for sharing posts and media",
+		tools: "React Firebase Tailwindcss",
+		github: "https://github.com/Valentine-chinedu/onstore_project/tree/master",
+		url: "https://onstore-project.firebaseapp.com/",
+		image: "/socialhangout screenshot.png",
+	},
+	{
+		Name: "Google-clone",
+		description:
+			"A clone of google search engine with full search functionality. ",
+		tools: "Nextjs Google-Api  Tailwindcss",
+		github: "https://github.com/Valentine-chinedu/onstore_project/tree/master",
+		url: "https://onstore-project.firebaseapp.com/",
+		image: "/google_search_clone.png",
+	},
+];
+
+export default { data };
