4eec6ec4bf02688a98d6335ac0d386ff66b01134
okosival <okosival@gmail.com>
2021-06-06 02:29:54 +0100

    changed text color

--- components/About.jsx
+++ components/About.jsx
@@ -1,24 +1,21 @@
 import Image from "next/image";
-import Zoom from "react-reveal/Zoom";
+import Slide from "react-reveal/Slide";
 
 function About() {
 	return (
 		<div className=' px-6 h-full pt-28 mb-8'>
-			<div className='flex items-center justify-center mb-8 overflow-x-hidden'>
-				<Zoom duration={1000} right>
-					<div className='bg-green-400 h-[0.15rem] w-[5.8rem]'></div>
-				</Zoom>
-				<Zoom duration={1000}>
-					<span className='font-bold text-2xl text-gray-800 mx-2'>
+			<Slide bottom>
+				<div className='flex items-center justify-center mb-8 overflow-x-hidden'>
+					<div className='bg-green-400 h-[0.15rem] w-[7rem]'></div>
+
+					<span className='font-bold text-2xl text-gray-100 mx-2'>
 						About Me
 					</span>
-				</Zoom>
-				<Zoom duration={1000} left>
-					<div className='bg-green-400 h-[0.15rem] w-[5.8rem]'></div>
-				</Zoom>
-			</div>
-			<Zoom duration={2000} bottom>
-				<div className='space-y-6 tracking-wider mb-8 overflow-y-hidden'>
+
+					<div className='bg-green-400 h-[0.15rem] w-[7rem]'></div>
+				</div>
+
+				<div className='space-y-6 tracking-wider mb-8 overflow-y-hidden text-gray-100'>
 					<p>
 						I am Valentine Chinedu. I am a web developer who enjoys building
 						engaging websites.
@@ -34,15 +31,16 @@ function About() {
 						development position with opportunities for continuous learning.
 					</p>
 				</div>
-			</Zoom>
-			<Image
-				className='rounded-md border-solid bo'
-				src='/photo-suite.png'
-				alt='man on suit'
-				width={400}
-				height={460}
-				layout='responsive'
-			/>
+
+				<Image
+					className='rounded-md border-solid bo'
+					src='/photo-suite.png'
+					alt='man on suit'
+					width={400}
+					height={460}
+					layout='responsive'
+				/>
+			</Slide>
 		</div>
 	);
 }
