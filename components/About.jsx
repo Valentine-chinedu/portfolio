9de1b9110251a2568d00e6f9615cbe08abacc7eb
okosival <okosival@gmail.com>
2021-06-12 17:29:46 +0100

    added padding to the left

--- components/About.jsx
+++ components/About.jsx
@@ -3,17 +3,19 @@ import Slide from "react-reveal/Slide";
 
 function About() {
 	return (
-		<div className='relative px-6 pb-12 h-full mb-8 text-gray-100'>
+		<div id='about' className='relative px-6 md:px-12 pb-12 h-full mb-8 pt-8'>
 			<Slide bottom duration={1000}>
 				<div className='flex items-center justify-center mb-8 overflow-x-hidden'>
-					<div className='bg-[#00ffff] h-[0.15rem] w-[7rem]'></div>
+					<div className='bg-[#00ffff] h-[0.15rem] w-[7rem] md:w-[15rem]'></div>
 
-					<span className='font-bold text-2xl mx-2'>About Me</span>
+					<span className='font-bold text-2xl text-gray-400 md:text-3xl mx-2 md:mx-4'>
+						About Me
+					</span>
 
-					<div className='bg-[#00ffff] h-[0.15rem] w-[7rem]'></div>
+					<div className='bg-[#00ffff] h-[0.15rem] w-[7rem] md:w-[15rem]'></div>
 				</div>
 
-				<div className='space-y-6 tracking-wider mb-8 overflow-y-hidden'>
+				<div className='space-y-6 tracking-wider mb-8 md:mb-14 pl-2 overflow-y-hidden md:text-lg text-gray-400'>
 					<p>
 						I am Valentine Chinedu. I am a web developer who enjoys building
 						engaging websites.
@@ -25,19 +27,19 @@ function About() {
 						satisfaction.
 					</p>
 					<p>
-						Currently, I am available for freelancing or a suitable web
-						development position with opportunities for continuous learning.
+						Currently, I am available for a suitable web development position or
+						freelancing, with opportunities for continuous learning.
 					</p>
 				</div>
-
-				<Image
-					className='rounded-md border-solid bo'
-					src='/photo-suite.png'
-					alt='man on suit'
-					width={400}
-					height={460}
-					layout='responsive'
-				/>
+				<div className='flex relative w-full justify-center'>
+					<div className='h-60 w-60 md:h-80 md:w-80 border-2 border-[#00ffff] rounded'></div>
+					<img
+						className='h-60 w-60 md:h-80 md:w-80 rounded object-cover absolute bottom-4 md:bottom-5 md:left-[9.5rem] right-[5rem]
+						'
+						src='photo-suite.png'
+						alt='png'
+					/>
+				</div>
 			</Slide>
 		</div>
 	);
