4fdadb4a15531661d95791d4074e0bdb2134d43e
okosival <okosival@gmail.com>
2021-06-12 17:34:55 +0100

    added padding bottom

--- components/project/Projects.jsx
+++ components/project/Projects.jsx
@@ -4,16 +4,14 @@ import Slide from "react-reveal/Slide";
 
 function Projects() {
 	return (
-		<div className='mb-16'>
+		<div id='projects' className='mb-16 pt-8'>
 			<Slide bottom duration={1000}>
 				<div className='flex items-center justify-center mb-8'>
-					<div className='bg-[#00ffff] h-[0.15rem] w-[7.5rem]'></div>
-
-					<span className='font-bold text-2xl text-gray-100 mx-2'>
+					<div className='bg-[#00ffff] h-[0.15rem] w-[8rem] md:w-[16rem]'></div>
+					<span className='font-bold text-2xl text-gray-400 md:text-3xl mx-2 md:mx-4 md:space-x-8'>
 						Projects
 					</span>
-
-					<div className='bg-[#00ffff] h-[0.15rem] w-[7.5rem]'></div>
+					<div className='bg-[#00ffff] h-[0.15rem] w-[8rem] md:w-[16rem]'></div>
 				</div>
 				{datas.map((data) => (
 					<div key={data._id}>
