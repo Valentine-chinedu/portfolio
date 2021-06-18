adc043572471373f659218e5d7e4f13dd4b60bd0
okosival <okosival@gmail.com>
2021-06-18 11:56:15 +0100

    solved the problem of slide animation

--- components/project/Projects.jsx
+++ components/project/Projects.jsx
@@ -1,24 +1,26 @@
-import datas from "../../data";
+import { projects } from "../../data";
 import ProjectDisplay from "./ProjectDisplay";
-import Slide from "react-reveal/Slide";
 
 function Projects() {
 	return (
-		<div id='projects' className='mb-16 pt-8'>
-			<Slide bottom duration={1000}>
-				<div className='flex items-center justify-center mb-8'>
-					<div className='bg-[#00ffff] h-[0.15rem] w-[8rem] md:w-[16rem]'></div>
-					<span className='font-bold text-2xl text-gray-400 md:text-3xl mx-2 md:mx-4 md:space-x-8'>
-						Projects
-					</span>
-					<div className='bg-[#00ffff] h-[0.15rem] w-[8rem] md:w-[16rem]'></div>
+		<div
+			id='projects'
+			className='mb-16 pt-8 lg:container overflow-hidden lg:mx-auto lg:px-80'
+		>
+			<div className='flex items-center justify-center mb-8 lg:mb-16'>
+				<div className='bg-[#00ffff] h-[0.10rem] w-[7rem] md:w-[15rem] lg:w-[23.5rem]'></div>
+
+				<span className='font-bold text-2xl text-gray-300 md:text-3xl mx-2 md:mx-4 md:space-x-8'>
+					Projects
+				</span>
+
+				<div className='bg-[#00ffff] h-[0.10rem] w-[7rem] md:w-[15rem] lg:w-[23.5rem]'></div>
+			</div>
+			{projects.map((project) => (
+				<div key={project._id} className='relative'>
+					<ProjectDisplay project={project} />
 				</div>
-				{datas.map((data) => (
-					<div key={data._id}>
-						<ProjectDisplay data={data} />
-					</div>
-				))}
-			</Slide>
+			))}
 		</div>
 	);
 }
