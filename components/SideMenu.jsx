ae6f3dc3c24891db874c7a2ff78ed7b2bc0d7236
okosival <okosival@gmail.com>
2021-07-07 18:54:50 +0100

    changed background color

--- components/SideMenu.jsx
+++ components/SideMenu.jsx
@@ -3,26 +3,24 @@ import { VscChromeClose } from "react-icons/vsc";
 function SideMenu({ openSideBar, setOpenSideBar }) {
 	return (
 		<div
-			className={`lg:hidden flex-col overflow-hidden fixed h-[46rem] md:h-screen z-50 inset-y-0 right-0 overflow-x-hidden bg-[#170030] transition-width duration-500 ease-in-out ${
+			className={`lg:hidden flex-col space-y-28 md:space-y-56 text-gray-400 overflow-hidden fixed h-[46rem] md:h-screen z-50 inset-y-0 right-0 overflow-x-hidden bg-[#120024] transition-width duration-500 ease-in-out ${
 				openSideBar ? "w-80 md:w-[26rem]" : "w-0"
 			}`}
 		>
 			<div
 				onClick={() => setOpenSideBar(false)}
-				className='flex w-full justify-end pt-6 pr-6'
+				className='flex w-full justify-end pt-4 pr-6'
 			>
-				<VscChromeClose className='text-4xl text-[#00ffff]' />
+				<VscChromeClose className='text-4xl md:text-5xl text-[#00ffff]' />
 			</div>
-			<div className='space-y-6 md:space-y-10 flex h-full flex-col justify-center md:text-lg'>
+			<div className='space-y-6 md:space-y-10 flex h-full flex-col md:text-lg'>
 				<a
 					onClick={() => setOpenSideBar(false)}
 					href='#about'
 					className='flex flex-col items-center'
 				>
 					<p className='text-[#00ffff] font-medium'>01.</p>
-					<h2 className='text-gray-200 font-semibold tracking-wider md:text-lg'>
-						About
-					</h2>
+					<h2 className='font-semibold tracking-wider md:text-lg'>About</h2>
 				</a>
 				<a
 					onClick={() => setOpenSideBar(false)}
@@ -30,9 +28,7 @@ function SideMenu({ openSideBar, setOpenSideBar }) {
 					className='flex flex-col items-center'
 				>
 					<p className='text-[#00ffff] font-medium'>02.</p>
-					<h2 className='text-gray-200 font-semibold tracking-wider md:text-lg'>
-						Skills
-					</h2>
+					<h2 className='font-semibold tracking-wider md:text-lg'>Skills</h2>
 				</a>
 				<a
 					onClick={() => setOpenSideBar(false)}
@@ -40,9 +36,7 @@ function SideMenu({ openSideBar, setOpenSideBar }) {
 					className='flex flex-col items-center'
 				>
 					<p className='text-[#00ffff] font-medium'>03.</p>
-					<h2 className='text-gray-200 font-semibold tracking-wider md:text-lg'>
-						Projects
-					</h2>
+					<h2 className='font-semibold tracking-wider md:text-lg'>Projects</h2>
 				</a>
 				<a
 					onClick={() => setOpenSideBar(false)}
@@ -50,14 +44,18 @@ function SideMenu({ openSideBar, setOpenSideBar }) {
 					className='flex flex-col items-center'
 				>
 					<p className='text-[#00ffff] font-medium'>04.</p>
-					<h2 className='text-gray-200 font-semibold tracking-wider md:text-lg mb-10'>
+					<h2 className='font-semibold tracking-wider md:text-lg mb-10'>
 						Contact
 					</h2>
 				</a>
 				<div className='flex flex-col items-center'>
-					<button className=' border-solid border-2 border-[#00ffff] text-[#00ffff] rounded px-12 py-3 font-semibold'>
+					<a
+						href='https://drive.google.com/file/d/1Ql5XyKJKs7cx_untCUSvKiWNKnsqbGKJ/view?usp=sharing'
+						target='_blank'
+						className=' border-solid border-2 border-[#00ffff] text-[#00ffff] rounded px-12 py-3 font-semibold'
+					>
 						Resume
-					</button>
+					</a>
 				</div>
 			</div>
 		</div>
