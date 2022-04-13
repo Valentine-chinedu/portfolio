d9a11337f0c4271e52c8c2889e30b4a1c4584990
okosival <valentine11.dev@gmail.com>
2022-04-13 14:14:15 +0100

    removed background color

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
-import Link from 'next/link';
-import { useContext, useState } from 'react';
+
+import { useContext } from 'react';
 import { BsEnvelope, BsGithub } from 'react-icons/bs';
 
 import { ImLinkedin } from 'react-icons/im';
@@ -13,11 +13,11 @@ const Footer = () => {
 
 	return (
 		<div
-			className={`flex h-48 flex-col items-center justify-center space-y-5 bg-gray-900 pt-20 pb-10 dark:bg-gray-700 md:space-y-8 md:py-28 lg:py-32 ${
+			className={`primary-theme dark:secondary-theme flex h-44 flex-col items-center justify-center space-y-5 pt-20 pb-10 md:space-y-8 md:py-28 lg:py-32 ${
 				openSideBar && 'blur-sm filter'
 			}`}
 		>
-			<div className='mb-4 space-x-4 text-sm text-gray-200 md:space-x-8 md:text-base'>
+			{/* <div className='mb-4 space-x-4 text-sm font-medium md:space-x-8 md:text-base'>
 				<AnchorLink
 					href='#home'
 					offset={() => 100}
@@ -60,28 +60,28 @@ const Footer = () => {
 				>
 					Contact
 				</AnchorLink>
-			</div>
-			<div className='md flex items-center space-x-8 lg:space-x-10'>
+			</div> */}
+			<div className='md flex items-center space-x-8  lg:space-x-10'>
 				<a
 					href='mailto:valentine11.dev@gmail.com'
 					target='_blank'
 					rel='noopener noreferrer'
 				>
-					<BsEnvelope className='text-3xl text-gray-300 hover:text-fuchsia-500 dark:hover:text-[#00FFFF] md:text-4xl' />
+					<BsEnvelope className='text-3xl text-gray-500 hover:text-fuchsia-500 dark:hover:text-[#00FFFF] md:text-4xl' />
 				</a>
 				<a
 					href='https://linkedin.com/in/valentine-okosi'
 					target='_blank'
 					rel='noopener noreferrer'
 				>
-					<ImLinkedin className='text-2xl text-gray-300 hover:text-fuchsia-500 dark:hover:text-[#00FFFF] md:text-3xl' />
+					<ImLinkedin className='text-2xl text-gray-500 hover:text-fuchsia-500 dark:hover:text-[#00FFFF] md:text-3xl' />
 				</a>
 				<a
 					href='https://github.com/Valentine-chinedu'
 					target='_blank'
 					rel='noopener noreferrer'
 				>
-					<BsGithub className='text-3xl text-gray-300 hover:text-fuchsia-500 dark:hover:text-[#00FFFF] md:text-4xl' />
+					<BsGithub className='text-3xl text-gray-500 hover:text-fuchsia-500 dark:hover:text-[#00FFFF] md:text-4xl' />
 				</a>
 				<a
 					className='relative h-8 w-8 md:h-10 md:w-10'
@@ -92,8 +92,8 @@ const Footer = () => {
 					<Image src='/brand-icon.png' layout='fill' />
 				</a>
 			</div>
-			<h3 className=' text-sm text-stone-100 md:text-base'>
-				&copy; 2022 Valentine Okosi Chinedu.
+			<h3 className=' text-sm font-medium text-gray-700 dark:text-gray-400 md:text-base'>
+				&copy; 2022 Chinedu Valentine Okosi.
 			</h3>
 		</div>
 	);
