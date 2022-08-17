684323238a6e221ee7357bb101625883c15acd78
Valentine <okosival@gmail.com>
2022-08-17 08:30:42 +0100

    changed navbar bg-color

--- components/header/NavBar.jsx
+++ components/header/NavBar.jsx
@@ -7,7 +7,7 @@ const NavBar = ({ openSideBar, setOpenSideBar }) => {
 	return (
 		<>
 			<nav
-				className={` fixed inset-y-0 right-0 flex flex-col items-center justify-center space-y-16 overflow-x-hidden bg-[#010e03] text-lg font-bold text-gray-50/80 md:space-y-20 md:text-xl lg:relative lg:h-10 lg:w-[35rem] lg:flex-row lg:space-x-8 lg:space-y-0 lg:bg-opacity-5  lg:text-sm ${
+				className={` fixed inset-y-0 right-0 flex flex-col items-center justify-center space-y-16 overflow-x-hidden bg-slate-900 text-lg font-bold text-gray-50/80 md:space-y-20 md:text-xl lg:relative lg:h-10 lg:w-[35rem] lg:flex-row lg:space-x-8 lg:space-y-0 lg:bg-[#010e03] lg:bg-opacity-5  lg:text-sm ${
 					openSideBar ? 'w-64 md:w-96' : 'w-0'
 				} z-50 h-full text-sm transition-[width] duration-500`}
 			>
